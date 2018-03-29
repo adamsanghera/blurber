168b5e8b8d1ef7ba7ef5f2880180edbb06f793eb
Adam Sanghera <adamsanghera@gmail.com>
2018-03-29 10:42:53 -0400

    removed login clutter

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	auth "github.com/adamsanghera/authenticator"
-	"github.com/adamsanghera/badge"
-)
-
-type LocalLogin struct {
-	auth   auth.Authenticator
-	minter badge.Minter
-}
-
 func main() {
-	a, err := auth.NewLocalPasswordAuth(0)
-	if err != nil {
-		panic(err)
-	}
 
-	l := LocalLogin{
-		auth:   a,
-		minter: badge.NewRandomTokenMinter(256),
-	}
 }
