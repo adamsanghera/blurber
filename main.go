60f05d15de8e9c9a95759177ae08e6d3139f9fd5
Adam Sanghera <adamsanghera@gmail.com>
2018-03-29 12:29:14 -0400

    first iter of login

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "log"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("./static/login/index.html")
+		t.Execute(w, nil)
+	}
+}
 
 func main() {
 	log.Printf("SERVER: Starting...")
 
+	// http.Handle("/", http.FileServer(http.Dir("./static/")))
+
+	http.HandleFunc("/", LoginHandler)
+
+	// http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":4000", nil)
 }
