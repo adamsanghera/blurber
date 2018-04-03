82ae355a94882a8acd44a745e695564f3d483102
Adam Sanghera <adamsanghera@gmail.com>
2018-04-03 18:22:29 -0400

    adding log statements to blurb, changing some names

--- blurb/blurb.go
+++ blurb/blurb.go
@@ -13,31 +13,42 @@ type Blurb struct {
 	CreatorName string
 }
 
-type BlurbLedger interface {
+// Ledger is the interface for a service that maintains
+// a record of blurbs (social media posts)
+type Ledger interface {
 	AddBlurb(creator string, b Blurb)
 	RemoveBlurb(creator string, b Blurb)
 }
 
-type LocalBlurbLedger struct {
+// LocalLedger is an implementation of Ledger, making use
+// of in-memory data structures to record blurbs
+type LocalLedger struct {
 	// Map from UID -> map [BID] -> Blurb
 	bidCounter int
 	ledger     map[int]map[int]Blurb
 }
 
-func NewLocalLedger() *LocalBlurbLedger {
-	return &LocalBlurbLedger{
+// NewLocalLedger returns a new and initialized LocalLedger
+func NewLocalLedger() *LocalLedger {
+	log.Printf("BLURB-LEDGER: Initializing")
+	return &LocalLedger{
 		ledger:     make(map[int]map[int]Blurb),
 		bidCounter: 0,
 	}
 }
 
-func (lbl *LocalBlurbLedger) AddNewBlurb(creatorID int, content string, creatorName string) {
+// AddNewBlurb consumes metadata to produce a new blurb object,
+// and stores that Blurb object in-memory.
+func (lbl *LocalLedger) AddNewBlurb(creatorID int, content string, creatorName string) {
+	log.Printf("BLURB-LEDGER: Adding a new blurb %d, created by %s", lbl.bidCounter, creatorName)
+
+	// Create a new mapping for a user, if one does not exist.
 	if _, exists := lbl.ledger[creatorID]; !exists {
 		lbl.ledger[creatorID] = make(map[int]Blurb)
 	}
 
+	// Birth the blurb
 	creationTime := time.Now()
-
 	lbl.ledger[creatorID][lbl.bidCounter] = Blurb{
 		Content:     content,
 		Time:        creationTime,
@@ -46,15 +57,19 @@ func (lbl *LocalBlurbLedger) AddNewBlurb(creatorID int, content string, creatorN
 		CreatorName: creatorName,
 	}
 
+	// Increment the BID counter
 	lbl.bidCounter++
-	log.Printf("Updated lbl: %v", lbl.ledger)
 }
 
-func (lbl *LocalBlurbLedger) RemoveBlurb(creatorID int, bid int) {
+// RemoveBlurb deletes a blurb from the ledger.
+func (lbl *LocalLedger) RemoveBlurb(creatorID int, bid int) {
+	log.Printf("BLURB-LEDGER: Deleting blurb %d", bid)
 	delete(lbl.ledger[creatorID], bid)
 }
 
-func (lbl *LocalBlurbLedger) GetUsrBlurb(creatorID int) []Blurb {
+// GetBlurbsCreatedBy returns all blurbs created by a given user.
+func (lbl *LocalLedger) GetBlurbsCreatedBy(creatorID int) []Blurb {
+	log.Printf("BLURB-LEDGER: Retrieving all blurbs by %d", creatorID)
 	bs := make([]Blurb, 0)
 	for _, v := range lbl.ledger[creatorID] {
 		bs = append(bs, v)
@@ -62,9 +77,10 @@ func (lbl *LocalBlurbLedger) GetUsrBlurb(creatorID int) []Blurb {
 	return bs
 }
 
-func (lbl *LocalBlurbLedger) RemoveAllBlurb(creatorID int) {
-    log.Printf("BLURB: Removing all blurbs by %s", creatorID)
-    for k := range lbl.ledger[creatorID] {
-        lbl.RemoveBlurb(creatorID, k)
-    }
+// RemoveAllBlurbsBy deletes all blurbs created by a given user.
+func (lbl *LocalLedger) RemoveAllBlurbsBy(creatorID int) {
+	log.Printf("BLURB-LEDGER: Removing all blurbs by %d", creatorID)
+	for k := range lbl.ledger[creatorID] {
+		lbl.RemoveBlurb(creatorID, k)
+	}
 }
