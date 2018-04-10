6a49eb9fafa1cb29fc7a12b06b1904b529ea0d44
Adam Sanghera <adamsanghera@gmail.com>
2018-04-10 12:32:32 -0400

    split file into service-specific pieces, added protobufs for the user db

--- protobufs/compile.sh
+++ protobufs/compile.sh
@@ -1,2 +1,4 @@
-rm ./blurber.pb.go > /dev/null 2>&1
-protoc ./blurber.proto --go_out=plugins=grpc:./
+rm ./blurb.pb.go > /dev/null 2>&1
+rm ./user.pb.go > /dev/null 2>&1
+protoc ./blurb.proto --go_out=plugins=grpc:./
+protoc ./user.proto --go_out=plugins=grpc:./
