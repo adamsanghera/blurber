2651da75c2137fbe3adf8a2959b481f0172644f0
Adam Sanghera <adamsanghera@gmail.com>
2018-04-10 12:52:01 -0400

    probably final commit for adding protobuf services and messages – changed the compile script quite a bit, and modified the .proto files to import commonly-used messages from a single .proto aptly-called common.proto

--- protobufs/compile.sh
+++ protobufs/compile.sh
@@ -1,4 +1,13 @@
-rm ./blurb.pb.go > /dev/null 2>&1
-rm ./user.pb.go > /dev/null 2>&1
-protoc ./blurb.proto --go_out=plugins=grpc:./
-protoc ./user.proto --go_out=plugins=grpc:./
+# Remove the dead weeds, if they exist
+rm -rf ./dist/*/* > /dev/null 2>&1
+
+# Make our path
+mkdir dist > /dev/null 2>&1
+mkdir dist/blurb > /dev/null 2>&1
+mkdir dist/user > /dev/null 2>&1
+mkdir dist/subscription > /dev/null 2>&1
+
+# Complie the files
+protoc ./blurb.proto --go_out=plugins=grpc:./dist/blurb/
+protoc ./user.proto --go_out=plugins=grpc:./dist/user/
+protoc ./subscription.proto --go_out=plugins=grpc:./dist/subscription/
