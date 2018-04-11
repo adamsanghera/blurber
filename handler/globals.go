6ce130d9688e55eae71ef0b474f7bb44557789b7
Adam Sanghera <adamsanghera@gmail.com>
2018-04-11 14:06:13 -0400

    Modified the globals and init file.  Now, instead of creating a local ledger and setting that to be the global var, we init a client to connect to the blurbDB gRPC server, and set that client to be the global var.  Now, we have to update our usage of BlurbDB to reflect the fact that we're using gRPC – lots of tedious commits to come!!!

--- handler/globals.go
+++ handler/globals.go
@@ -1,11 +1,12 @@
 package handler
 
 import (
-	"github.com/adamsanghera/blurber/blurb"
+	blurb "github.com/adamsanghera/blurber/protobufs/dist/blurb"
 	reg "github.com/adamsanghera/blurber/registration"
 	sub "github.com/adamsanghera/blurber/subscription"
 )
 
 var userDB = reg.NewLocalLedger()
-var blurbDB = blurb.NewLocalLedger()
+
+var blurbDB blurb.BlurbDBClient
 var subDB = sub.NewLocalLedger()
