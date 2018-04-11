91ef5c54624df9d137565beda1da99ac1ea2d1c8
Adam Sanghera <adamsanghera@gmail.com>
2018-04-11 14:04:25 -0400

    Added a driver, to spin up a gRPC server for the blurb DB.  Read notes in the updated README.md to see how to use the contents of this folder. `run.sh` is a script that contains env variables to set before spinning up this server

---
+++ blurb-driver/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc/reflection"
+
+	"github.com/adamsanghera/blurber/blurb"
+	blurbpb "github.com/adamsanghera/blurber/protobufs/dist/blurb"
+
+	"google.golang.org/grpc"
+)
+
+func createListenAddr() string {
+	listenHost := os.Getenv("BLURB_HOST")
+	listenPort := os.Getenv("BLURB_PORT")
+
+	if listenHost == "" {
+		panic("No hostname set")
+	}
+	if listenPort == "" {
+		panic("No port number set")
+	}
+
+	ret, err := net.LookupHost(listenHost)
+	if err != nil {
+		panic(err)
+	}
+
+	return ret[0] + ":" + listenPort
+}
+
+func main() {
+	addr := createListenAddr()
+
+	log.Printf("BlurbServer: Derived address: (%s)", addr)
+
+	srv := blurb.NewLedgerServer(addr)
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	blurbpb.RegisterBlurbDBServer(s, srv)
+
+	log.Printf("BlurbServer: Starting on (%s)", addr)
+
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %s\n", err.Error())
+	}
+}
