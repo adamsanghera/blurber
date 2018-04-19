7002f3b04ff9e559e3a63443342be51501940d58
Adam Sanghera <adamsanghera@gmail.com>
2018-04-19 13:40:15 -0400

    added a test file testing concurrent logins, checkins and checkous.  Alles gut.  Made the package threadsafe

--- registration/registration.go
+++ registration/registration.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"log"
+	"sync"
 	"time"
 )
 
@@ -27,24 +28,33 @@ type Token struct {
 // LocalLedger is an implementation of Ledger, which uses in-memory data
 // structures to support Ledger's functionality.
 type LocalLedger struct {
-	userSet  map[int32]user
-	userID   map[string]int32
-	pwdMap   map[int32]string
-	tokenMap map[int32]Token
+	userSet  sync.Map
+	userID   sync.Map
+	pwdMap   sync.Map
+	tokenMap sync.Map
 
 	uidCounter int32
+	uidMut     sync.Mutex
 }
 
 // NewLocalLedger initializes and returns a new LocalLedger instance.
 func NewLocalLedger() *LocalLedger {
 	log.Printf("AUTH-LEDGER: Initializing")
 	return &LocalLedger{
-		userSet:    make(map[int32]user),
-		userID:     make(map[string]int32),
-		pwdMap:     make(map[int32]string),
-		tokenMap:   make(map[int32]Token),
+		userSet:    sync.Map{},
+		userID:     sync.Map{},
+		pwdMap:     sync.Map{},
+		tokenMap:   sync.Map{},
 		uidCounter: 0,
 	}
+}
+
+func (ll *LocalLedger) measureUID() int32 {
+	ll.uidMut.Lock()
+	ret := ll.uidCounter
+	ll.uidCounter++
+	ll.uidMut.Unlock()
+	return ret
 }
 
 // AddNewUser integrates a new user into the ledger, with 3 operations.
@@ -52,46 +62,51 @@ func NewLocalLedger() *LocalLedger {
 //   (2) Instantiation of a new User object, associated with the UID
 //   (3) Storage of a provided password, associated with the UID.
 // The ledger's UID counter is then (TODO: thread-safely) incremented.
-func (lul *LocalLedger) AddNewUser(name string, pwd string) error {
+func (ll *LocalLedger) AddNewUser(name string, pwd string) error {
 	log.Printf("AUTH-LEDGER: Registering new user %s", name)
 
-	if _, exists := lul.userID[name]; exists {
+	if _, exists := ll.userID.Load(name); exists {
 		return errors.New("A user with that name already exists")
 	}
 
-	lul.userSet[lul.uidCounter] = user{
-		Name: name,
-		UID:  lul.uidCounter,
-	}
-	lul.userID[name] = lul.uidCounter
-	lul.pwdMap[lul.uidCounter] = pwd
+	id := ll.measureUID()
 
-	// TODO: Make threadsafe
-	lul.uidCounter++
+	ll.userSet.Store(id, user{
+		Name: name,
+		UID:  id,
+	})
+
+	ll.userID.Store(name, id)
+	ll.pwdMap.Store(id, pwd)
 
 	return nil
 }
 
 // LogIn performs a username-check, followed by a password-check.
 // If both checks pass, then a new token is allocated to associated UID, and returned.
-func (lul *LocalLedger) LogIn(uname string, pwd string) (string, error) {
+func (ll *LocalLedger) LogIn(uname string, pwd string) (string, error) {
 	log.Printf("AUTH-LEDGER: Logging-in %s", uname)
 
 	// username-check
-	id, err := lul.GetUserID(uname)
+	id, err := ll.GetUserID(uname)
 	if err != nil {
 		return "", err
 	}
 
 	// password-check
-	if lul.pwdMap[id] != pwd {
+	pass, exists := ll.pwdMap.Load(id)
+	if !exists {
+		panic("Password for user doesn't exist!")
+	}
+
+	if pass != pwd {
 		return "", errors.New("Bad password for " + uname)
 	}
 
-	return lul.allocateNewToken(id), nil
+	return ll.allocateNewToken(id), nil
 }
 
-func (lul *LocalLedger) allocateNewToken(uid int32) string {
+func (ll *LocalLedger) allocateNewToken(uid int32) string {
 	// Generate a random token of fixed-length
 	bitString := make([]byte, 256)
 	_, err := rand.Read(bitString)
@@ -101,78 +116,102 @@ func (lul *LocalLedger) allocateNewToken(uid int32) string {
 	token := hex.EncodeToString(bitString)
 
 	// Associate the token with the given uid
-	lul.tokenMap[uid] = Token{token: token, creationDate: time.Now()}
+	ll.tokenMap.Store(uid, Token{token: token, creationDate: time.Now()})
+
+	return token
+}
+
+func (ll *LocalLedger) getToken(uid int32) Token {
+	tok, exists := ll.tokenMap.Load(uid)
+	if !exists {
+		return Token{}
+	}
+
+	token, ok := tok.(Token)
+	if !ok {
+		panic("Bad token in DB")
+	}
 
 	return token
 }
 
 // CheckIn performs a username-check and a token-check.
 // If both checks pass, then a new token is validated and returned
-func (lul *LocalLedger) CheckIn(uname string, token string) (string, error) {
+func (ll *LocalLedger) CheckIn(uname string, token string) (string, error) {
 	log.Printf("AUTH-LEDGER: Checking-in %s", uname)
 
 	// username-check
-	id, err := lul.GetUserID(uname)
+	id, err := ll.GetUserID(uname)
 	if err != nil {
 		return "", err
 	}
 
+	internalToken := ll.getToken(id)
+
 	// token-check: part 1
-	if lul.tokenMap[id].token != token {
+	if internalToken.token != token {
 		return "", errors.New("Bad token for " + uname)
 	}
 
 	// token-check: part 2
-	if time.Since(lul.tokenMap[id].creationDate) > 30*time.Minute {
+	if time.Since(internalToken.creationDate) > 30*time.Minute {
 		return "", errors.New("Session expried for " + uname)
 	}
 
-	return lul.allocateNewToken(id), nil
+	return ll.allocateNewToken(id), nil
 }
 
-func (lul *LocalLedger) CheckOut(uname string, token string) error {
+func (ll *LocalLedger) CheckOut(uname string, token string) error {
 	log.Printf("AUTH-LEDGER: Checking-out %s", uname)
 
 	// username-check
-	id, err := lul.GetUserID(uname)
+	id, err := ll.GetUserID(uname)
 	if err != nil {
 		return err
 	}
 
+	internalToken := ll.getToken(id)
+
 	// token-check
-	if lul.tokenMap[id].token != token {
+	if internalToken.token != token {
 		return errors.New("Bad token for " + uname)
 	}
 
-	delete(lul.tokenMap, id)
+	ll.tokenMap.Delete(id)
 
 	return nil
 }
 
 // GetUserID retrieves the permanent UID associated with uname.
-func (lul *LocalLedger) GetUserID(uname string) (int32, error) {
+func (ll *LocalLedger) GetUserID(uname string) (int32, error) {
 	log.Printf("AUTH-LEDGER: Retrieving UID for %s", uname)
 
-	id, ok := lul.userID[uname]
+	id, ok := ll.userID.Load(uname)
 	if !ok {
 		return -1, errors.New("No record of " + uname + " exists")
 	}
-	return id, nil
+
+	intID, ok := id.(int32)
+	if !ok {
+		panic("Bad id value stored in map")
+	}
+
+	return intID, nil
 }
 
 // Remove eliminates all history of uname, including its UID.
-func (lul *LocalLedger) Remove(uname string) error {
+func (ll *LocalLedger) Remove(uname string) error {
 	log.Printf("AUTH-LEDGER: Removing all history of %s", uname)
 
 	// username-check
-	id, err := lul.GetUserID(uname)
+	id, err := ll.GetUserID(uname)
 	if err != nil {
 		return err
 	}
 
-	delete(lul.userID, uname)
-	delete(lul.userSet, id)
-	delete(lul.pwdMap, id)
-	delete(lul.tokenMap, id)
+	ll.userSet.Delete(uname)
+	ll.userSet.Delete(id)
+	ll.pwdMap.Delete(id)
+	ll.tokenMap.Delete(id)
 	return nil
 }
