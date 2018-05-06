f98acd9b822d5db262a524e978b3a6b2199c84df
Adam Sanghera <adamsanghera@gmail.com>
2018-05-06 16:02:20 -0400

    updating protobuf vendor

--- subscription-driver/run.sh
+++ subscription-driver/run.sh
@@ -12,3 +12,5 @@ echo "Binary built, executing procs..."
 SUB_PORT=6000 LEADER_ADDRESS=0 ./subscription-driver &> ./logs/leader.txt &disown
 SUB_PORT=6010 LEADER_ADDRESS=127.0.0.1:6001 ./subscription-driver &> ./logs/follower1.txt &disown
 SUB_PORT=6020 LEADER_ADDRESS=127.0.0.1:6001 ./subscription-driver &> ./logs/follower2.txt &disown
+
+echo "Procs are running"
