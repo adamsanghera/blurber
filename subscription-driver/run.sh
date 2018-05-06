ebe9a0a9d7d74888287468547441dd9adc038976
Adam Sanghera <adamsanghera@gmail.com>
2018-05-06 15:54:47 -0400

    moving logs into folder, ignoring them

--- subscription-driver/run.sh
+++ subscription-driver/run.sh
@@ -1,6 +1,7 @@
 echo "Cleaning up old logs"
 
-rm ./*.txt > /dev/null
+rm -rf logs &> /dev/null
+mkdir logs
 
 echo "Building the binary"
 
@@ -8,6 +9,6 @@ go build ./
 
 echo "Binary built, executing procs..."
 
-SUB_PORT=6000 LEADER_ADDRESS=0 ./subscription-driver &> leader.txt &disown
-SUB_PORT=6010 LEADER_ADDRESS=127.0.0.1:6001 ./subscription-driver &> follower1.txt &disown
-SUB_PORT=6020 LEADER_ADDRESS=127.0.0.1:6001 ./subscription-driver &> follower2.txt &disown
+SUB_PORT=6000 LEADER_ADDRESS=0 ./subscription-driver &> ./logs/leader.txt &disown
+SUB_PORT=6010 LEADER_ADDRESS=127.0.0.1:6001 ./subscription-driver &> ./logs/follower1.txt &disown
+SUB_PORT=6020 LEADER_ADDRESS=127.0.0.1:6001 ./subscription-driver &> ./logs/follower2.txt &disown
